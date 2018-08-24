c566e8e83d15bb9b15510b72ffe4443fc419ff15
Deivid <deividlalessio@gmail.com>
2018-08-24 14:37:59 -0300

    Add files via upload

---
+++ Exercício 2.py
@@ -0,0 +1,8 @@
+n1 = float(input('Digite sua N1: '))
+n2 = float(input('Digite sua N2: '))
+if (n1+n2)/2 == 10:
+    print('Aprovado com Distinção')
+elif (n1+n2)/2 >= 7:
+    print('Aprovado')
+else:
+    print('Reprovado')
---
+++ Exercício 4.py
@@ -0,0 +1,20 @@
+num1 = input('Digite um numero: ')
+num2 = input('Digite outro numero: ')
+num3 = input('Digite um terceiro numero: ')
+if num1>num2 and num1>num3:
+    print +num1
+elif num2>num1 and num2>num3:
+    print +num2
+elif num3>num2 and num3>num1:
+    print +num3
+else:
+    print('Houve um empate triplo!')
+    
+if num1<num2 and num1<num3:
+    print +num1
+elif num2<num1 and num2<num3:
+    print +num2
+elif num3<num2 and num3<num1:
+    print +num3
+else:
+    print('Houve um empate triplo!')
